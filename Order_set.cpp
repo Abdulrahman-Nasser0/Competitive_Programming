72ed29aa03224298718c1d3812dc24d860100ba0
Abdulrahman Nasser <123002074+Abdulrahman-Nasser0@users.noreply.github.com>
2023-10-11 20:59:45 +0300

    Create Order_set
    Find how many numbers are less than x in a set

---
+++ Order_set.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int  long long
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<>, rb_tree_tag,tree_order_statistics_node_update>// set
+
+typedef tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+#define ll long long
+void myerase(ordered_set &t, int v){
+    int rank = t.order_of_key(v);//Number of elements that are less than v in t
+    ordered_set::iterator it = t.find_by_order(rank); //Iterator that points to the (rank+1)th element in t
+    t.erase(it);
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    int n ;cin >> n;
+    vector<int> a(n), b(n); 
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    vector<int> m(n);
+    for (int  i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+
+    ordered_multiset s;
+    long long ans  = 0;
+    for(int i = 0; i < n ; ++i ) {
+		ans += s.order_of_key(a[i]-b[i]);
+        s.insert(b[i]-a[i]);
+	}
+    cout << ans << endl;
+    
+}
